271cd2567e82e0bfae86257a8970ff5e2e5e8dde
KuangPF <1633397286@qq.com>
2023-03-05 17:23:38 +0800

    feat(core): initialize qiankun plugin

--- packages/core/.umirc.ts
+++ packages/core/.umirc.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "umi";
+import { defineConfig } from 'umi';
 
 export default defineConfig({
   routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: '/', component: 'index' },
+    { path: '/docs', component: 'docs' },
   ],
+  plugins: ['@umijs/plugins/dist/qiankun'],
+  qiankun: {
+    master: {},
+  },
   npmClient: 'npm',
 });
---
+++ packages/core/src/app.ts
@@ -0,0 +1,12 @@
+export const qiankun = {
+  apps: [
+    {
+      name: 'app1',
+      entry: '//localhost:7001',
+    },
+    {
+      name: 'app2',
+      entry: '//localhost:7002',
+    },
+  ],
+};
