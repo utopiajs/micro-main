cb4b05f9b283f6831f15f7cd93404204bb1c7cf8
KuangPF <1633397286@qq.com>
2023-03-08 20:27:41 +0800

    chore(user-center): set base path

--- packages/user-center/.umirc.ts
+++ packages/user-center/.umirc.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   publicPath: '/user-center/',
-  routes: [{ path: '/user-center/base-info', component: 'base-info' }],
+  base: '/user-center/',
+  routes: [{ path: '/base-info', component: 'base-info' }],
   plugins: ['@umijs/plugins/dist/qiankun'],
   qiankun: {
     slave: {},
