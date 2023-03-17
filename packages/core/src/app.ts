a04e569e653491ecc4ccc26c109fc8c9cf2877a1
KuangPF <1633397286@qq.com>
2023-03-17 20:35:19 +0800

    feat(core): initialize layout

--- packages/core/src/app.ts
+++ packages/core/src/app.ts
@@ -1,3 +1,5 @@
+import { type RunTimeLayoutConfig } from '@umijs/max';
+
 export const qiankun = {
   apps: [
     {
@@ -5,4 +7,11 @@ export const qiankun = {
       entry: '//localhost:9001'
     }
   ]
+};
+
+export const layout: RunTimeLayoutConfig = () => {
+  return {
+    title: '微前端主平台',
+    layout: 'mix'
+  };
 };
