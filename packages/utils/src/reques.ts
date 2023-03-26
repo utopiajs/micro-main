6c19111ba410c4c02477e10eb9b2d1de8c0f5e5c
KuangPF <1633397286@qq.com>
2023-03-26 15:08:25 +0800

    feat(utils): initialize base request

---
+++ packages/utils/src/reques.ts
@@ -0,0 +1,32 @@
+// 基于 axios 封装请求库
+import axios from 'axios';
+import { getToken } from './auth';
+
+const TIME_OUT = 1 * 60 * 1000;
+
+const request = () => {
+  const requestInstance = axios.create({
+    baseURL: '',
+    timeout: TIME_OUT
+  });
+
+  requestInstance.interceptors.request.use(
+    (config) => {
+      if (getToken()) {
+        config.headers.Authorization = getToken();
+      }
+      return config;
+    },
+    (error) => {
+      // do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  requestInstance.interceptors.response.use((response) => {
+    console.log(response);
+    return response;
+  });
+};
+
+export default request;
