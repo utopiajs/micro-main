f1f048881e717c0d3c2b857507fbdf62edcd9fc0
KuangPF <1633397286@qq.com>
2023-04-17 21:35:40 +0800

    feat(user-center): add `ConfigProvider`

--- packages/user-center/src/layouts/index.tsx
+++ packages/user-center/src/layouts/index.tsx
@@ -1,10 +1,17 @@
+import { ConfigProvider, theme } from 'antd';
 import { Outlet } from 'umi';
 import styles from './index.less';
 
 export default function Layout() {
   return (
-    <div className={styles['user-center-main-content']}>
-      <Outlet />
-    </div>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.defaultAlgorithm
+      }}
+    >
+      <div className={styles['user-center-main-content']}>
+        <Outlet />
+      </div>
+    </ConfigProvider>
   );
 }
