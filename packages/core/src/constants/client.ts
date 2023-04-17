1262c231c2a16dad09ac1bfcbbf285b4b7723d0b
KuangPF <1633397286@qq.com>
2023-04-17 22:09:59 +0800

    feat(core): extract client constant

---
+++ packages/core/src/constants/client.ts
@@ -0,0 +1,11 @@
+import { IClientConstantProps } from '@/types';
+
+const CLIENT_NAME = '微前端主平台';
+const CLIENT_VERSION = '1.0.0';
+
+const clientConstantProps: IClientConstantProps = {
+  clientName: CLIENT_NAME,
+  clientVersion: CLIENT_VERSION
+};
+
+export { clientConstantProps };
---
+++ packages/core/src/types/client.type.ts
@@ -0,0 +1,8 @@
+interface IClientConstantProps {
+  /** 平台名称 */
+  clientName: string;
+  /** 平台版本号 */
+  clientVersion: string;
+}
+
+export { IClientConstantProps };
