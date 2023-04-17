16b2cbe761bc1bef9b460834f3ee7958ae017350
KuangPF <1633397286@qq.com>
2023-04-18 00:00:38 +0800

    feat(user-center): add copyright label in login panel

--- packages/user-center/src/pages/login/index.tsx
+++ packages/user-center/src/pages/login/index.tsx
@@ -4,7 +4,8 @@ import { useSiteToken } from '@utopia/micro-main-utils';
 import { Button, Form, Input, Typography } from 'antd';
 import styles from './index.less';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
+
 const LoginPage = () => {
   const {
     token: { colorBgContainer, padding, boxShadow }
@@ -20,34 +21,39 @@ const LoginPage = () => {
         }}
       >
         <Title level={4}>微前端主平台</Title>
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: '用户名是必填项' }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: '密码是必填项' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+        <Form name="user-center-login" className="login-form">
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: '用户名是必填项' }]}
           >
-            登陆
-          </Button>
-        </Form.Item>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="用户名"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '密码是必填项' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="密码"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              登陆
+            </Button>
+          </Form.Item>
+        </Form>
+        <p>
+          <Text type="secondary">KuangPF | Copyright © 2022-present</Text>
+        </p>
       </div>
     </div>
   );
