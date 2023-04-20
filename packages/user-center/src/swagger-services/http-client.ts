e9b37ac87f662213a6d0a6eee5bf8ef658462330
KuangPF <1633397286@qq.com>
2023-04-20 20:02:56 +0800

    feat(services): update core swagger services

---
+++ packages/user-center/src/swagger-services/http-client.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import { HttpClient, type RequestParams } from '@utopia/micro-main-utils';
+
+interface ResponseCommonType<T> {
+  message: string;
+  errorCode: string;
+  data: T;
+}
+
+export { HttpClient, RequestParams, ResponseCommonType };
