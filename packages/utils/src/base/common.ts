db441b5cd6403d8d18a400f1415fed1726b770a2
KuangPF <1633397286@qq.com>
2023-04-20 20:12:26 +0800

    feat(utils): add `getQueryParams` common method

--- packages/utils/src/base/common.ts
+++ packages/utils/src/base/common.ts
@@ -2,7 +2,21 @@ import Cookies from 'js-cookie';
 import { ERROR_CODE } from '../constants';
 
 const isDev = () => process.env.NODE_ENV?.trim() !== 'production';
+
+const getQueryParams = (url: string = window.location.search) => {
+  const params: {
+    [key: string]: string;
+  } = {};
+
+  url.replace(/([^?&]+)=([^&]+)/g, (_, k: string, v: string) => {
+    params[k] = v;
+    return '';
+  });
+
+  return params;
+};
+
 const isApiSuccess = (errCode: string) => errCode === ERROR_CODE;
 const _Cookies = Cookies;
 
-export { isDev, isApiSuccess, _Cookies };
+export { isDev, getQueryParams, isApiSuccess, _Cookies };
