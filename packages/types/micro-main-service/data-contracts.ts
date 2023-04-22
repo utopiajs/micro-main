02486a8f3aeda223af9b12faeec72c134aa6edd2
KuangPF <1633397286@qq.com>
2023-04-22 17:00:55 +0800

    feat(script): copy micro common types

---
+++ packages/types/micro-main-service/data-contracts.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+/** @example {"id":"5ebac534954b54139806c112","email":"fake@example.com","name":"fake name","role":"user"} */
+export interface User {
+  id?: string;
+  /** @format email */
+  email?: string;
+  name?: string;
+  role?: 'user' | 'admin';
+}
+
+/** @example {"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWJhYzUzNDk1NGI1NDEzOTgwNmMxMTIiLCJpYXQiOjE1ODkyOTg0ODQsImV4cCI6MTU4OTMwMDI4NH0.m1U63blB0MLej_WfB7yC2FTMnCziif9X8yzwDEfJXAg","expires":"2020-05-12T16:18:04.793Z"} */
+export interface Token {
+  token?: string;
+  /** @format date-time */
+  expires?: string;
+}
+
+export interface AuthTokens {
+  access?: Token;
+  refresh?: Token;
+}
+
+export interface BingImg {
+  startdate?: string;
+  fullstartdate?: string;
+  enddate?: string;
+  url?: string;
+  urlbase?: string;
+  copyright?: string;
+  copyrightlink?: string;
+  quiz?: string;
+  wp?: string;
+}
+
+export interface Error {
+  code?: number;
+  message?: string;
+}
--- scripts/gen-core-services.js
+++ scripts/gen-core-services.js
@@ -1,13 +1,22 @@
+/* eslint-disable no-console */
 const { generateApi } = require('swagger-typescript-api');
 const path = require('path');
+const fs = require('fs');
 
 const apis = [
   {
-    output: path.resolve(process.cwd(), './packages/core/src/swagger-services/'),
-    url: 'http://localhost:3000/api/micro-main/v1/docs/swagger.json'
+    output: path.resolve(
+      process.cwd(),
+      './packages/core/src/swagger-services/'
+    ),
+    url: 'http://localhost:3000/api/micro-main/v1/docs/swagger.json',
+    name: 'core'
   },
   {
-    output: path.resolve(process.cwd(), './packages/user-center/src/swagger-services/'),
+    output: path.resolve(
+      process.cwd(),
+      './packages/user-center/src/swagger-services/'
+    ),
     url: 'http://localhost:3000/api/micro-main/v1/docs/swagger.json'
   }
 ];
@@ -25,6 +34,27 @@ apis?.forEach((api, index) => {
       moduleNameFirstTag: true, // apis htt-client data-contracts
       unwrapResponseData: true,
       generateUnionEnums: true
+    }).then(() => {
+      if (api.name === 'core') {
+        // copy data-contracts types
+        fs.copyFile(
+          path.resolve(
+            process.cwd(),
+            './packages/core/src/swagger-services/data-contracts.ts'
+          ),
+          path.resolve(
+            process.cwd(),
+            './packages/types/micro-main-service/data-contracts.ts'
+          ),
+          (error) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log('data-contracts done');
+            }
+          }
+        );
+      }
     });
   }, index * 1500);
 });
