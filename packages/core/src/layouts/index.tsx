4988d3eb83af0f31dfe75447779535da06cb547d
KuangPF <1633397286@qq.com>
2023-04-26 23:07:25 +0800

    feat(core): add antd `ConfigProvider` for global config

--- packages/core/src/layouts/index.tsx
+++ packages/core/src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderRightContent } from '@/components';
 import { ProLayout, type MenuDataItem } from '@ant-design/pro-components';
 import { Link, Outlet, useLocation, useModel } from '@umijs/max';
+import { ConfigProvider, theme } from 'antd';
 import { useCallback, useMemo, type ReactNode } from 'react';
 
 type MenuItemProps = MenuDataItem & {
@@ -33,32 +34,38 @@ export default function Layout() {
     [location.pathname]
   );
   return (
-    <ProLayout
-      title={initialState?.client.clientName}
-      layout="mix"
-      siderWidth={208}
-      pure={Boolean(!initialState?.currentUser.id)}
-      rightContentRender={() => headerContentJSX}
-      menuItemRender={menuItemRender}
-      menu={{
-        request: () =>
-          new Promise((resolve) => {
-            resolve([
-              {
-                name: '用户管理',
-                path: '/user-center/',
-                children: [
-                  {
-                    name: '用户信息',
-                    path: '/user-center/base-info'
-                  }
-                ]
-              }
-            ]);
-          })
+    <ConfigProvider
+      theme={{
+        algorithm: theme.defaultAlgorithm
       }}
     >
-      <Outlet />
-    </ProLayout>
+      <ProLayout
+        title={initialState?.client.clientName}
+        layout="mix"
+        siderWidth={208}
+        pure={Boolean(!initialState?.currentUser.id)}
+        rightContentRender={() => headerContentJSX}
+        menuItemRender={menuItemRender}
+        menu={{
+          request: () =>
+            new Promise((resolve) => {
+              resolve([
+                {
+                  name: '用户管理',
+                  path: '/user-center/',
+                  children: [
+                    {
+                      name: '用户信息',
+                      path: '/user-center/base-info'
+                    }
+                  ]
+                }
+              ]);
+            })
+        }}
+      >
+        <Outlet />
+      </ProLayout>
+    </ConfigProvider>
   );
 }
