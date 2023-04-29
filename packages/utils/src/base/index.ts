662f2f6db44cbadad73e9c24c8c6fcff614418ed
KuangPF <1633397286@qq.com>
2023-04-29 20:57:51 +0800

    feat(utils): add PublishSubscribe

--- packages/utils/src/base/index.ts
+++ packages/utils/src/base/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export * from './common';
 export * from './request';
+export * from './publish-subscribe';
---
+++ packages/utils/src/base/publish-subscribe.ts
@@ -0,0 +1,47 @@
+/** 发布订阅者模式 */
+
+class PublishSubscribe {
+  events: {
+    [key: string]: Array<Function>;
+  };
+
+  constructor() {
+    this.events = {};
+  }
+
+  // 订阅方法
+  public subscribe(type, callback) {
+    if (!this.events[type]) {
+      this.events[type] = [];
+    }
+    this.events[type].push(callback);
+  }
+
+  // 发布方法
+  public publish(type, ...args) {
+    if (this.events[type]) {
+      this.events[type].forEach((callback) => callback(...args));
+    }
+  }
+
+  // 取消订阅方法
+  public unSubscribe(type, callback) {
+    if (this.events[type]) {
+      const callbackIndex = this.events[type].findIndex((e) => e === callback);
+      if (callbackIndex !== -1) {
+        this.events[type].splice(callbackIndex, 1);
+      }
+    }
+    if (this.events[type].length === 0) {
+      delete this.events[type];
+    }
+  }
+
+  public unSubscribeAll(type) {
+    if (this.events[type]) {
+      delete this.events[type];
+    }
+  }
+}
+
+export { PublishSubscribe };
