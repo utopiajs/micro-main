6f81239bfa57cfbebfe1d64f1ee868f79503722d
KuangPF <1633397286@qq.com>
2023-05-02 20:47:18 +0800

    feat(user-center): initialize preference setting module

--- packages/user-center/src/pages/preference-setting/index.tsx
+++ packages/user-center/src/pages/preference-setting/index.tsx
@@ -1,8 +1,22 @@
 /** 偏好设置 */
+import { Form } from 'antd';
 import { type FC } from 'react';
 
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 }
+};
+
 const PreferenceSetting: FC = () => {
-  return <div>偏好设置</div>;
+  return (
+    <div>
+      <Form name="preference-strring" {...formItemLayout}>
+        <Form.Item label="主题风格">
+          <span className="ant-form-text">浅色模式</span>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default PreferenceSetting;
