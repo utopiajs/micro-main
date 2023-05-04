2089228b9dbf584201664b5f8a2cef9aa25a6262
KuangPF <1633397286@qq.com>
2023-05-04 22:43:02 +0800

    chore(eslint-config): remove react/prop-types config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ['@utopia/eslint-config-react', '@utopia/eslint-config-react/hooks'],
   rules: {
     'import/no-extraneous-dependencies': 0,
-    'no-param-reassign': 0
+    'no-param-reassign': 0,
+    'react/prop-types': 0
   },
   globals: {
     jsdom: true,
