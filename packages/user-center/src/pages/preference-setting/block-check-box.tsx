adfe0d2dfcfbfbfd85a6aef372fd7e107146c2d6
KuangPF <1633397286@qq.com>
2023-05-04 21:52:25 +0800

    feat(user-center): theme site Form.Item

---
+++ packages/user-center/src/pages/preference-setting/block-check-box.tsx
@@ -0,0 +1,57 @@
+import { CheckOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
+
+import React, { useEffect, useState } from 'react';
+
+export interface BlockCheckboxProps {
+  value: string;
+  onChange: (key: string) => void;
+  list?: {
+    title: string;
+    key: string;
+    url: string;
+  }[];
+}
+
+const BlockCheckbox: React.FC<BlockCheckboxProps> = ({
+  value,
+  onChange,
+  list
+}) => {
+  const baseClassName = 'user-center-block-checkbox';
+  const [dom, setDom] = useState<JSX.Element[]>([]);
+  useEffect(() => {
+    const domList = (list || []).map((item) => (
+      <div
+        key={item.key}
+        className={`${baseClassName}-item`}
+        onClick={() => onChange(item.key)}
+      >
+        <Tooltip title={item.title} key={item.key}>
+          <img src={item.url} alt={item.key} />
+        </Tooltip>
+        <div
+          className={`${baseClassName}-selectIcon`}
+          style={{
+            display: value === item.key ? 'block' : 'none'
+          }}
+        >
+          <CheckOutlined />
+        </div>
+      </div>
+    ));
+    setDom(domList);
+  }, [value, list, baseClassName, onChange]);
+  return (
+    <div
+      className={baseClassName}
+      style={{
+        minHeight: 42
+      }}
+    >
+      {dom}
+    </div>
+  );
+};
+
+export default BlockCheckbox;
--- packages/user-center/src/pages/preference-setting/index.tsx
+++ packages/user-center/src/pages/preference-setting/index.tsx
@@ -1,18 +1,57 @@
 /** 偏好设置 */
-import { Form } from 'antd';
-import { type FC } from 'react';
+import { Form, type MenuTheme } from 'antd';
+import { useCallback, useRef, useState, type FC } from 'react';
+import BlockCheckbox from './block-check-box';
+import Styles from './index.less';
 
+export interface IPreferenceSettings {
+  navTheme: MenuTheme;
+}
 const formItemLayout = {
-  labelCol: { span: 6 },
-  wrapperCol: { span: 14 }
+  labelCol: { span: 4 },
+  wrapperCol: { span: 20 }
 };
 
+const themeList = [
+  {
+    key: 'light',
+    url: 'https://gw.alipayobjects.com/zos/antfincdn/NQ%24zoisaD2/jpRkZQMyYRryryPNtyIC.svg',
+    title: '亮色菜单风格'
+  },
+  {
+    key: 'dark',
+    url: 'https://gw.alipayobjects.com/zos/antfincdn/XwFOFbLkSM/LCkqqYNmvBEbokSDscrm.svg',
+    title: '暗色风格'
+  }
+];
+
 const PreferenceSetting: FC = () => {
+  const [settingState, setSettingState] = useState<IPreferenceSettings>({
+    navTheme: 'dark'
+  });
+
+  const preStateRef = useRef(settingState);
+
+  const handleSetSettingState = useCallback((key: string, value: string) => {
+    const nextState = { ...preStateRef.current };
+    nextState[key] = value;
+    preStateRef.current = nextState;
+    setSettingState(nextState);
+  }, []);
+
+  const { navTheme } = settingState;
   return (
-    <div>
+    <div className={Styles['preference-setting-wrap']}>
       <Form name="preference-strring" {...formItemLayout}>
         <Form.Item label="主题风格">
-          <span className="ant-form-text">浅色模式</span>
+          <BlockCheckbox
+            list={themeList}
+            value={navTheme}
+            key="navTheme"
+            onChange={(value) => {
+              handleSetSettingState('navTheme', value);
+            }}
+          />
         </Form.Item>
       </Form>
     </div>
