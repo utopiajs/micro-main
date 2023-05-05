ff702ab943f56daa57eb4290ede8f647139fffb5
KuangPF <1633397286@qq.com>
2023-05-05 23:48:05 +0800

    feat(core): initialize site-theme value

---
+++ packages/core/src/constants/site-theme.ts
@@ -0,0 +1,11 @@
+import { theme, type ThemeConfig } from 'antd';
+
+const siteThemeConfig: ThemeConfig = {
+  algorithm: theme.defaultAlgorithm,
+  token: {
+    colorPrimary: '#1677FF',
+    borderRadius: 4
+  }
+};
+
+export { siteThemeConfig };
