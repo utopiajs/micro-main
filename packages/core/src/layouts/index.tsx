9a23c4f2a0184fa11aa9a27f46add1ab053a073a
KuangPF <1633397286@qq.com>
2023-05-05 23:44:17 +0800

    feat(core): handle theme change logic

--- packages/core/src/layouts/index.tsx
+++ packages/core/src/layouts/index.tsx
@@ -1,8 +1,15 @@
 import { HeaderRightContent } from '@/components';
 import { ProLayout, type MenuDataItem } from '@ant-design/pro-components';
 import { Link, Outlet, useLocation, useModel, useNavigate } from '@umijs/max';
-import { ConfigProvider, theme } from 'antd';
-import { useCallback, useMemo, type ReactNode } from 'react';
+import { PUB_SUB_TYPES } from '@utopia/micro-types';
+import { ConfigProvider, theme as antdTheme, type ThemeConfig } from 'antd';
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  type ReactNode
+} from 'react';
 
 type MenuItemProps = MenuDataItem & {
   isUrl: boolean;
@@ -11,9 +18,14 @@ type MenuItemProps = MenuDataItem & {
 
 export default function Layout() {
   const { initialState } = useModel('@@initialState');
+  const [siteThemeConfig, setSiteThemeConfig] = useState(
+    initialState?.siteThemeConfig
+  );
+
   const location = useLocation();
   const navigate = useNavigate();
   const headerContentJSX = useMemo(() => <HeaderRightContent />, []);
+
   const menuItemRender = useCallback(
     (menuItemProps: MenuItemProps, defaultDom: ReactNode) => {
       if (menuItemProps.isUrl || menuItemProps.children) {
@@ -34,12 +46,38 @@ export default function Layout() {
     },
     [location.pathname]
   );
+
+  const getSiteThemeConfig = useCallback((siteTheme) => {
+    const { theme, colorPrimary, borderRadius } = siteTheme;
+    const nextSiteThemeConfig: ThemeConfig = {
+      algorithm:
+        theme === 'light'
+          ? antdTheme.defaultAlgorithm
+          : antdTheme.darkAlgorithm,
+      token: {
+        colorPrimary,
+        borderRadius
+      }
+    };
+
+    return nextSiteThemeConfig;
+  }, []);
+
+  useEffect(() => {
+    window._MICRO_MAIN_CORE_PUB_SUB_.subscribe(
+      PUB_SUB_TYPES.GET_SITE_THEME_VALUE,
+      (payload) => {
+        setSiteThemeConfig(payload);
+        window._MICRO_MAIN_CORE_PUB_SUB_.publis(
+          PUB_SUB_TYPES.UPDATE_SITE_THEME_CONFIG,
+          getSiteThemeConfig(payload)
+        );
+      }
+    );
+  }, [getSiteThemeConfig]);
+
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.defaultAlgorithm
-      }}
-    >
+    <ConfigProvider theme={siteThemeConfig}>
       <ProLayout
         title={initialState?.client.clientName}
         layout="mix"
