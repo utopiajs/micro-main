1e54f8f77dd4490693ea3768a7a5d4c2c0726fe7
KuangPF <1633397286@qq.com>
2023-05-07 23:40:27 +0800

    feat(user-center): layout `ConfigProvider` 适配默认 siteThemeConfig

--- packages/user-center/src/layouts/index.tsx
+++ packages/user-center/src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import { Outlet, useModel } from '@umijs/max';
+import { getAntdConfigProviderTheme } from '@utopia/micro-main-utils';
 import { PUB_SUB_TYPES, type IInitialState } from '@utopia/micro-types';
 import { ConfigProvider } from 'antd';
 import { useEffect, useState } from 'react';
@@ -8,21 +9,21 @@ import styles from './index.less';
 export default function Layout() {
   const { initialState }: { initialState: IInitialState } =
     useModel('@@qiankunStateFromMaster') || {};
-  const [siteThemeConfig, setSiteThemeConfig] = useState(
-    initialState?.siteThemeConfig
+  const [siteAntdThemeConfig, setSiteAntdThemeConfig] = useState(
+    getAntdConfigProviderTheme(initialState?.siteThemeConfig)
   );
 
   useEffect(() => {
     window._MICRO_MAIN_CORE_PUB_SUB_?.subscribe(
       PUB_SUB_TYPES.UPDATE_SITE_THEME_CONFIG,
       (payload) => {
-        setSiteThemeConfig(payload);
+        setSiteAntdThemeConfig(payload);
       }
     );
   }, []);
 
   return (
-    <ConfigProvider theme={siteThemeConfig}>
+    <ConfigProvider theme={siteAntdThemeConfig}>
       <div className={styles['user-center-main-content']}>
         <Outlet />
       </div>
