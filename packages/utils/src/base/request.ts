3dea428029a9ddc737829d2791a6d82ff6513a11
KuangPF <1633397286@qq.com>
2023-06-19 00:07:33 +0800

    chore(utils): request util add User field

--- packages/utils/src/base/request.ts
+++ packages/utils/src/base/request.ts
@@ -7,6 +7,7 @@ import axios, {
   type InternalAxiosRequestConfig
 } from 'axios';
 import { getToken } from './auth';
+import { _Cookies } from './common';
 
 interface IRawResponseData {
   /** 接口内部错误码，反应接口具体错误信息，'0000000000' 表示响应成功 */
@@ -27,7 +28,9 @@ export interface FullRequestParams
   extends AxiosRequestConfig,
     RestRequestConfig {}
 
-export interface ApiConfig extends AxiosRequestConfig {}
+export interface ApiConfig extends AxiosRequestConfig {
+  customApi?: string;
+}
 export type RequestParams = Omit<
   FullRequestParams,
   'data' | 'method' | 'params' | 'url'
@@ -83,6 +86,8 @@ class HttpClient {
 
         // token
         config.headers.Authorization = `Bearer ${getToken()}`;
+        // user info
+        config.headers.User = `usercode:${_Cookies.get('id')}`;
         return config;
       }
     );
@@ -112,7 +117,7 @@ class HttpClient {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-  public request = <T = any, _E = any>(
+  public request = <T = unknown, _E = unknown>(
     requestParams: FullRequestParams
   ): Promise<T> => {
     const { showErrorMessage } = requestParams;
