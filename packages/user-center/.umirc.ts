77b8ee9715a89048bc7fa7663b472d88b105677d
KuangPF <1633397286@qq.com>
2023-06-24 23:52:04 +0800

    chore(uc): update mfsu value

--- packages/user-center/.umirc.ts
+++ packages/user-center/.umirc.ts
@@ -3,20 +3,8 @@ import { defineConfig } from '@umijs/max';
 export default defineConfig({
   mfsu: {
     strategy: 'normal',
-    shared: {
-      react: {
-        singleton: true,
-        eager: true
-      },
-      'react-dom': {
-        singleton: true,
-        eager: true
-      }
-    }
   },
-  monorepoRedirect: {
-    srcDir: ['dist/esm']
-  },
+  monorepoRedirect: {},
   publicPath: '/', //https://github.com/umijs/qiankun/issues/1953
   base: '/user-center/',
   routes: [
