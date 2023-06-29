0389c0121a68a04bf67efaf77e06bbebc60ee94d
KuangPF <1633397286@qq.com>
2023-06-30 00:08:49 +0800

    feat(ucenter): CoreTable API 完善

--- packages/user-center/src/components/core-table/index.tsx
+++ packages/user-center/src/components/core-table/index.tsx
@@ -1,44 +1,71 @@
 // based on antd table
 import { COMPONENT_CLASSNAME_PREFIX } from '@/constants/component';
 import { useSiteToken } from '@utopia/micro-main-utils';
-import type { PaginationProps, TableProps } from 'antd';
-import { Button, Table } from 'antd';
-import { useCallback, useEffect, useState } from 'react';
+import { Input, Pagination, PaginationProps, Table, TableProps } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import './index.less';
 
 const prefixCls = COMPONENT_CLASSNAME_PREFIX;
+const EMPTY_LIST: any[] = [];
+const EMPTY_PAGINATION: PaginationProps = {};
+const coreTableEleId = 'core-table-id';
 
 type CreateDataSourceType<RecordType> = {
   data: RecordType[];
-  pagination?: PaginationProps;
+  pagination: PaginationProps;
 };
 
 export interface CoreTableProps<RecordType>
   extends Omit<TableProps<RecordType>, 'dataSource'> {
-  createDataSource: () => Promise<CreateDataSourceType<RecordType>>;
+  createDataSource?: () => Promise<CreateDataSourceType<RecordType>>;
   /* 是否展示序列号 */
   showSerialNumber?: boolean;
+  /** 头部操作栏配置 */
+  headerOptionBar?: React.ReactNode[];
 }
 
 function CoreTable<RecordType extends object = any>(
   props: CoreTableProps<RecordType>
 ) {
   const {
-    createDataSource = () => ({ data: [] }),
+    createDataSource,
     columns,
     showSerialNumber,
+    headerOptionBar,
     ...restProps
   } = props;
 
   const [dataSource, setDataSource] = useState<RecordType[]>([]);
+  const [pagination, setPagination] = useState<PaginationProps>({});
+  const [coreTableScroll, setCoreTableScroll] = useState<
+    TableProps<RecordType>['scroll']
+  >({});
   const {
-    token: { paddingXS, marginXS }
+    token: {
+      paddingXS,
+      marginXS,
+      marginXXS,
+      colorBgElevated,
+      fontSize,
+      borderRadius,
+      colorPrimary,
+      colorBgLayout,
+      colorBorderSecondary,
+      colorText
+    }
   } = useSiteToken();
 
   const getTableDataSource = useCallback(async () => {
-    const { data } = await createDataSource();
-    setDataSource(data);
+    let nextDataSource = EMPTY_LIST;
+    let nextPagination = EMPTY_PAGINATION;
+    if (createDataSource) {
+      const { data, pagination: _pagination } = await createDataSource();
+      nextDataSource = data;
+      nextPagination = _pagination;
+    }
+    setDataSource(nextDataSource);
+    setPagination(nextPagination);
   }, [createDataSource]);
 
   const getNextColums = useCallback(() => {
@@ -56,28 +83,92 @@ function CoreTable<RecordType extends object = any>(
 
   const [nextColums] = useState(getNextColums());
 
+  const getCoreTableScroll = useCallback(() => {
+    const coreTableContent = document.getElementById(coreTableEleId);
+    if (coreTableContent) {
+      setCoreTableScroll({
+        y: coreTableContent.getBoundingClientRect().height - 47
+      });
+    }
+  }, []);
+
   useEffect(() => {
     getTableDataSource();
-  }, [getTableDataSource]);
+    getCoreTableScroll();
+  }, [getTableDataSource, getCoreTableScroll]);
 
   return (
-    <div className={`${prefixCls}-core-table`}>
+    <div
+      className={`${prefixCls}-core-table`}
+      style={{ backgroundColor: colorBgLayout }}
+    >
       <div
         className={`${prefixCls}-core-table-header`}
-        style={{ padding: `${paddingXS}px 0`, marginBottom: marginXS }}
+        style={{
+          padding: `${paddingXS}px`,
+          backgroundColor: colorBgElevated,
+          borderTopLeftRadius: borderRadius,
+          borderTopRightRadius: borderRadius,
+          border: `1px solid ${colorBorderSecondary}`
+        }}
       >
-        <Button>新增</Button>
-        <Button>编辑</Button>
+        <div className={`${prefixCls}-core-table-header-options`}>
+          {headerOptionBar?.map((item, index) => (
+            <div
+              className="header-operation-item"
+              key={index}
+              style={{
+                marginLeft: `${index === 0 ? '0' : `${marginXS}`}`
+              }}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className={`${prefixCls}-core-table-header-search`}>
+          <Input.Search placeholder="请输入用户名称、邮箱等" allowClear />
+        </div>
       </div>
-      <div className={`${prefixCls}-core-table-content`}>
+      <div
+        className={`${prefixCls}-core-table-content`}
+        id={coreTableEleId}
+        style={{ margin: `${marginXXS}px 0` }}
+      >
         <Table<RecordType>
           {...restProps}
           pagination={false}
           dataSource={dataSource}
           columns={nextColums}
+          scroll={coreTableScroll}
         />
       </div>
-      <div className={`${prefixCls}-core-table-footer`}>footer</div>
+      <div
+        className={`${prefixCls}-core-table-footer`}
+        style={{
+          padding: `${paddingXS}`,
+          backgroundColor: colorBgElevated,
+          borderBottomLeftRadius: borderRadius,
+          borderBottomRightRadius: borderRadius,
+          border: `1px solid ${colorBorderSecondary}`
+        }}
+      >
+        <div style={{ fontSize, color: colorText }}>
+          <span>共</span>
+          <span
+            style={{
+              color: colorPrimary,
+              margin: marginXXS,
+              fontWeight: 'bold'
+            }}
+          >
+            {pagination.total}
+          </span>
+          <span>条记录</span>
+        </div>
+        <div>
+          <Pagination showQuickJumper showSizeChanger {...pagination} />
+        </div>
+      </div>
     </div>
   );
 }
