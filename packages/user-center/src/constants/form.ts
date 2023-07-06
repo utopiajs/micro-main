ce3b72cd4b65884094d1c9ab56d8ee376db83443
KuangPF <1633397286@qq.com>
2023-07-06 22:44:15 +0800

    feat(ucenter): formItemLayout

---
+++ packages/user-center/src/constants/form.ts
@@ -0,0 +1,13 @@
+const formItemLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 20 }
+};
+const formItemEditLayout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 6 }
+};
+const formTailLayout = {
+  wrapperCol: { offset: 4, span: 20 }
+};
+
+export { formItemLayout, formTailLayout, formItemEditLayout };
