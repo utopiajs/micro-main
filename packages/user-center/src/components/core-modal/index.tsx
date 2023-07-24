c586092a5b24aff658eb80cd289a3cd66c9e28fa
KuangPF <1633397286@qq.com>
2023-07-24 20:21:53 +0800

    feat(uc): 完善 `CoreModal` 相关 api

---
+++ packages/user-center/src/components/core-modal/index.tsx
@@ -0,0 +1,82 @@
+// 基于 ant modal 定制封装
+
+import { COMPONENT_CLASSNAME_PREFIX } from '@/constants/component';
+import { CloseOutlined } from '@ant-design/icons';
+import { useHoverStyle, useSiteToken } from '@utopia/micro-main-utils';
+import type { ModalProps } from 'antd';
+import { Modal } from 'antd';
+import classNames from 'classnames';
+import React, { useCallback } from 'react';
+
+import './index.less';
+
+interface CoreModalProps extends ModalProps {
+  height?: number;
+}
+
+const prefixCls = COMPONENT_CLASSNAME_PREFIX;
+
+const CoreModal: React.FC<CoreModalProps> = (props) => {
+  const {
+    title,
+    children,
+    className,
+    height,
+    closable,
+    onCancel,
+    ...restProps
+  } = props;
+  const {
+    token: { paddingXS, colorBorderSecondary, colorBgTextHover }
+  } = useSiteToken();
+
+  useHoverStyle(
+    `.${prefixCls}-core-modal-close-icon`,
+    {},
+    { backgroundColor: colorBgTextHover }
+  );
+  const handleCancel = useCallback(
+    (e) => {
+      if (onCancel) {
+        onCancel(e);
+      }
+    },
+    [onCancel]
+  );
+
+  return (
+    <Modal
+      title={title}
+      closable={false}
+      onCancel={handleCancel}
+      {...restProps}
+      className={classNames([className, `${prefixCls}-core-modal`])}
+    >
+      {closable && (
+        <div
+          className={`${prefixCls}-core-modal-close-icon`}
+          title="关闭弹窗"
+          onClick={handleCancel}
+        >
+          <CloseOutlined />
+        </div>
+      )}
+      {title ? (
+        <div
+          className={`${prefixCls}-core-modal-header`}
+          style={{ padding: paddingXS, borderColor: colorBorderSecondary }}
+        >
+          {title}
+        </div>
+      ) : null}
+      <div
+        className={`${prefixCls}-core-modal-body`}
+        style={{ height, padding: paddingXS }}
+      >
+        {children}
+      </div>
+    </Modal>
+  );
+};
+
+export default CoreModal;
