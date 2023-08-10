4f8a194080023996cd174bf94eb5a4e6b76782cc
KuangPF <1633397286@qq.com>
2023-08-10 23:37:38 +0800

    refactor(ucenter): 使用 `useAntdProviderConfigTheme` hook 重构主题切换逻辑

--- packages/user-center/src/layouts/index.tsx
+++ packages/user-center/src/layouts/index.tsx
@@ -1,29 +1,15 @@
 import qiankunStateFromMaster from '@/mock/qiankunStateFromMaster';
 import { Outlet, useModel } from '@umijs/max';
-import {
-  getAntdConfigProviderTheme,
-  PUB_SUB_TYPES
-} from '@utopia/micro-main-utils';
-import type { QiankunStateForSlaveProps } from '@utopia/micro-types';
+import { useAntdProviderConfigTheme } from '@utopia/micro-main-utils';
+import type { QiankunStateFromMasterProps } from '@utopia/micro-types';
 import { ConfigProvider } from 'antd';
-import { useEffect, useState } from 'react';
 import OutletWrap from './outlet-wrap';
 
 export default function Layout() {
-  const { initialState }: QiankunStateForSlaveProps =
+  const { qiankunGlobalState }: QiankunStateFromMasterProps =
     useModel('@@qiankunStateFromMaster') || qiankunStateFromMaster;
-  const [siteAntdThemeConfig, setSiteAntdThemeConfig] = useState(
-    getAntdConfigProviderTheme(initialState?.siteThemeConfig)
-  );
-
-  useEffect(() => {
-    window._MICRO_MAIN_CORE_PUB_SUB_?.subscribe(
-      PUB_SUB_TYPES.UPDATE_SITE_THEME_CONFIG,
-      (payload) => {
-        setSiteAntdThemeConfig(payload);
-      }
-    );
-  }, []);
+  const { siteAntdThemeConfig } =
+    useAntdProviderConfigTheme(qiankunGlobalState);
 
   return (
     <ConfigProvider theme={siteAntdThemeConfig}>
