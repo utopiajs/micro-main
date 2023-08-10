14db5e1c0082e8df34177b5efe075653c2364057
KuangPF <1633397286@qq.com>
2023-08-11 00:06:11 +0800

    chore(core): remove useless code

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './client';
 export * from './site-theme';
-export * from './user';
--- packages/core/src/constants/user.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@utopia/micro-types';
-
-const DEFAULT_USER_INFO: User = {
-  id: '',
-  preferenceSetting: {}
-};
-
-export { DEFAULT_USER_INFO };
