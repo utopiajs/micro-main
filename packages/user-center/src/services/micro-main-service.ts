8610e1381a9c55c006d845f5e5aa7d512c7a3e7b
KuangPF <1633397286@qq.com>
2023-08-17 20:44:31 +0800

    feat(ucenter): add coreMenuApi

--- packages/user-center/src/services/micro-main-service.ts
+++ packages/user-center/src/services/micro-main-service.ts
@@ -1,11 +1,13 @@
 import { Auth } from '@/swagger-services/Auth';
 import { Commons } from '@/swagger-services/Commons';
+import { Menus } from '@/swagger-services/Menus';
 import { Roles } from '@/swagger-services/Roles';
 import { Users } from '@/swagger-services/Users';
 
 const coreUserApi = new Users();
 const coreAuthApi = new Auth();
 const coreRoleApi = new Roles();
+const coreMenuApi = new Menus();
 const coreCommonsApi = new Commons();
 
-export { coreUserApi, coreAuthApi, coreRoleApi, coreCommonsApi };
+export { coreUserApi, coreAuthApi, coreMenuApi, coreRoleApi, coreCommonsApi };
