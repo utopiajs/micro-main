b55c546fa17b52c03b2222c83a03df32ec04ab43
KuangPF <1633397286@qq.com>
2023-09-12 20:20:55 +0800

    feat(core): 侧边栏菜单支持外部链接模式

--- packages/core/src/components/core-pro-layout/index.tsx
+++ packages/core/src/components/core-pro-layout/index.tsx
@@ -1,16 +1,22 @@
-import { coreMenuApi } from '@/services';
 import { convertMenuData } from '@/utils';
 import type { MenuDataItem } from '@ant-design/pro-components';
 import { ProLayout } from '@ant-design/pro-components';
 import {
+  history,
   Link,
   useLocation,
   useModel,
   useNavigate,
   useOutlet
 } from '@umijs/max';
-import { isApiSuccess, useSiteToken } from '@utopia/micro-main-utils';
-import React, { useCallback, useMemo, useState } from 'react';
+import { useSiteToken } from '@utopia/micro-main-utils';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
 import HeaderRightContent from '../header-right-content';
 import Styles from './index.less';
 import MenuCollapseButton from './menu-collapse-button';
@@ -23,7 +29,9 @@ type MenuItemProps = MenuDataItem & {
 
 const CoreProLayout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const { initialState } = useModel('@@initialState');
+  const [menuSelectedKeys, setMenuSelectedKeys] = useState<string[]>([]); // 菜单选择 keys，受控，主要适配外部链接
+  const { qiankunGlobalState } = useModel('@@qiankunStateForSlave');
+
   const {
     token: { colorPrimary, controlItemBgActive, colorBgElevated }
   } = useSiteToken();
@@ -31,11 +39,45 @@ const CoreProLayout: React.FC = () => {
   const navigate = useNavigate();
   const outlet = useOutlet();
 
+  const menuActionRef = useRef<{
+    reload: () => void;
+  }>();
+
+  const { client, menuConfigUserTree, currentUser } = qiankunGlobalState;
   const headerContentJSX = useMemo(() => <HeaderRightContent />, []);
+
+  const handleIframeLink = useCallback((menu) => {
+    setMenuSelectedKeys([menu.key]);
+    if (menu.target === '_blank') {
+      window.open(menu.path, '_blank');
+    } else {
+      history.push(`/micro-main-core/iframe/${encodeURIComponent(menu.path)}`);
+    }
+  }, []);
+
+  const handlePageChange = useCallback((_location) => {
+    const selectedKey = _location.pathname;
+    if (!selectedKey.startsWith('/micro-main-core/iframe/')) {
+      setMenuSelectedKeys([selectedKey]);
+    }
+  }, []);
 
   const menuItemRender = useCallback(
     (menuItemProps: MenuItemProps, defaultDom: React.ReactNode) => {
-      if (menuItemProps.isUrl || menuItemProps.children) {
+      // eg. http://www.xxx.com
+      if (menuItemProps.isUrl) {
+        return (
+          <div
+            className="ant-menu-submenu-url"
+            onClick={() => {
+              handleIframeLink(menuItemProps);
+            }}
+          >
+            <div style={{ pointerEvents: 'none' }}>{defaultDom}</div>
+          </div>
+        );
+      }
+      if (menuItemProps.children) {
         return defaultDom;
       }
       if (menuItemProps.path && location.pathname !== menuItemProps.path) {
@@ -49,23 +91,30 @@ const CoreProLayout: React.FC = () => {
           </Link>
         );
       }
+
       return defaultDom;
     },
-    [location.pathname]
+    [location.pathname, handleIframeLink]
   );
+
+  useEffect(() => {
+    menuActionRef.current?.reload();
+  }, [menuConfigUserTree]);
 
   return (
     <ProLayout
-      title={initialState?.client.clientName}
+      title={client.clientName}
       layout="mix"
       siderWidth={208}
-      logo={initialState?.client.logo}
-      pure={Boolean(!initialState?.currentUser.id)}
+      logo={client.logo}
+      pure={Boolean(!currentUser.id)}
       onMenuHeaderClick={(e) => {
         e.stopPropagation();
         e.preventDefault();
         navigate('/');
       }}
+      selectedKeys={menuSelectedKeys}
+      onPageChange={handlePageChange}
       rightContentRender={() => headerContentJSX}
       menuItemRender={menuItemRender}
       collapsedButtonRender={false}
@@ -75,14 +124,12 @@ const CoreProLayout: React.FC = () => {
         <MenuCollapseButton collapsed={collapsed} onCollapse={setCollapsed} />
       ]}
       className={Styles['micro-main-core-layout']}
+      actionRef={menuActionRef}
       menu={{
+        locale: false,
         request: async () => {
-          const { errorCode, data } = await coreMenuApi.menuUserTreeWithGet();
-          if (isApiSuccess(errorCode)) {
-            const convertedMenuData = convertMenuData(data);
-            return convertedMenuData;
-          }
-          return [];
+          const convertedMenuData = convertMenuData(menuConfigUserTree || []);
+          return convertedMenuData;
         }
       }}
       token={{
