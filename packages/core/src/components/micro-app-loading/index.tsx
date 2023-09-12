969c3f42abb67965e31670ade47437d3a8f61b83
KuangPF <1633397286@qq.com>
2023-09-12 20:23:30 +0800

    style(core): 调整 `MicroAppLoading` 样式

--- packages/core/src/components/micro-app-loading/index.tsx
+++ packages/core/src/components/micro-app-loading/index.tsx
@@ -4,7 +4,9 @@ import Styles from './index.less';
 const MicroAppLoading = (loading) => {
   return loading ? (
     <div className={Styles['micro-app-loading-wrap']}>
-      <Spin tip="应用加载中..." spinning />
+      <Spin tip="应用加载中..." spinning>
+        <div className="content" />
+      </Spin>
     </div>
   ) : null;
 };
