bcd0c20528a66acf555bed5325956ea7bde0f718
KuangPF <1633397286@qq.com>
2023-09-17 20:23:37 +0800

    feat(service): add `coreClientConfig`

--- packages/core/src/services/micro-main-service.ts
+++ packages/core/src/services/micro-main-service.ts
@@ -1,9 +1,11 @@
 import { Auth } from '@/swagger-services/Auth';
 import { Menus } from '@/swagger-services/Menus';
 import { Users } from '@/swagger-services/Users';
+import { ClientConfigs } from '@/swagger-services/ClientConfigs';
 
 const coreUserApi = new Users();
 const coreAuthApi = new Auth();
 const coreMenuApi = new Menus();
+const coreClientConfig = new ClientConfigs();
 
-export { coreUserApi, coreAuthApi, coreMenuApi };
+export { coreUserApi, coreAuthApi, coreMenuApi, coreClientConfig };
