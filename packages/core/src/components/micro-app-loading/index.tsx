f5b544cbcc5dbffb34330ecbf358ec31e2ba589e
KuangPF <1633397286@qq.com>
2023-09-17 20:37:13 +0800

    feat(core): 优化自定义 Loading 加载逻辑

--- packages/core/src/components/micro-app-loading/index.tsx
+++ packages/core/src/components/micro-app-loading/index.tsx
@@ -1,8 +1,21 @@
+import { useLocation } from '@umijs/max';
+import {
+  ROUTE_LOGIN_PATH,
+  ROUTE_REGISTER_PATH
+} from '@utopia/micro-main-utils';
 import { Spin } from 'antd';
+import { useCallback } from 'react';
 import Styles from './index.less';
 
 const MicroAppLoading = (loading) => {
-  return loading ? (
+  const { pathname } = useLocation();
+  const showLoadingComponent = useCallback(() => {
+    return (
+      ![ROUTE_LOGIN_PATH, ROUTE_REGISTER_PATH].includes(pathname) && loading
+    );
+  }, [pathname, loading]);
+
+  return showLoadingComponent() ? (
     <div className={Styles['micro-app-loading-wrap']}>
       <Spin tip="应用加载中..." spinning>
         <div className="content" />
