1dfb632b6e59da6c5eff596eee7b993286627386
KuangPF <1633397286@qq.com>
2023-09-17 17:26:42 +0800

    feat(deps): add `@utopia/core-component`

---
+++ packages/core-component/src/index.ts
@@ -0,0 +1,7 @@
+import CoreModal from './components/core-modal';
+import CoreTable from './components/core-table';
+import CoreTree from './components/core-tree';
+import NotFound from './components/not-found';
+import TitleLabel from './components/title-label';
+
+export { TitleLabel, CoreTable, CoreTree, CoreModal, NotFound };
