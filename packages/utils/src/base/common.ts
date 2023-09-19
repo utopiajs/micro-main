8bf6417596e711be18a361442a0b31dd36204462
KuangPF <1633397286@qq.com>
2023-09-19 20:59:25 +0800

    feat(uitls): add isUrl 判断是否为绝对路径链接

--- packages/utils/src/base/common.ts
+++ packages/utils/src/base/common.ts
@@ -70,12 +70,31 @@ function renderDefaultField(value, fillText = '--') {
   return value || fillText;
 }
 
+/**
+ * 判断是不是一个 url
+ * @param  {string|undefined} path
+ * @returns boolean
+ */
+const isUrl = (path: string | undefined): boolean => {
+  if (!path) return false;
+  if (!path.startsWith('http')) {
+    return false;
+  }
+  try {
+    const url = new URL(path);
+    return !!url;
+  } catch (error) {
+    return false;
+  }
+};
+
 export {
   isDev,
   getQueryParams,
   isApiSuccess,
   _Cookies,
   uuidv4,
+  isUrl,
   removeEmptyFields,
   convertArrayFromTree,
   renderDefaultField
