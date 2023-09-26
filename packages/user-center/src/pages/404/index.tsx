9b07021665581c753f7ee80c98817d3bd896965f
KuangPF <1633397286@qq.com>
2023-09-27 00:26:41 +0800

    feat(component): core and ucenter use `NotFound` component

--- packages/user-center/src/pages/404/index.tsx
+++ packages/user-center/src/pages/404/index.tsx
@@ -1,24 +1,10 @@
-import trexIcon from '@/assets/images/trex-icon.png';
-import { Typography } from 'antd';
+import { NotFound } from '@utopia/core-component';
 import Styles from './index.less';
 
 const NotFoundPage = () => {
   return (
     <div className={Styles['not-found-page']}>
-      <img src={trexIcon} alt="" width={100} />
-      <div className="main-message">
-        <Typography.Title level={4}>
-          未找到该页面，可能原因如下：
-        </Typography.Title>
-        <ul className="suggestions-summary-list">
-          <li>
-            <Typography.Text>平台中不存在该页面模块</Typography.Text>
-          </li>
-          <li>
-            <Typography.Text>暂无权限访问该模块</Typography.Text>
-          </li>
-        </ul>
-      </div>
+      <NotFound />
     </div>
   );
 };
